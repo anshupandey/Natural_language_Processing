abcbcd023430c9f88161b4efa1cdb39ae85e84de
Anshu <anshuspandey@gmail.com>
2020-02-07 01:02:34 +0400

    chatbot dummy code

--- chatbot/Chatbot_train.py
+++ chatbot/Chatbot_train.py
@@ -8,7 +8,6 @@ Created on Fri Feb  7 00:02:40 2020
 ##########################################################################
 # Data Preparation
 
-import os
 import json
 data = open("data\qus_data.txt","r").read()
 data = data.replace("'", "\"")
@@ -47,8 +46,8 @@ y = np.array(y)
 ##########################################################################
 ### Training intent recognition model
         
-MAX_SEQUENCE_LENGTH = 20
-MAX_WORDS = 1000
+MAX_SEQUENCE_LENGTH = round((sum(doc_size)/len(doc_size) + max(doc_size))/2)+1
+MAX_WORDS = 500
 EMBEDDING_DIM = 30
 
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -67,6 +66,8 @@ xdata = pad_sequences(sequences,maxlen=MAX_SEQUENCE_LENGTH)
 from tensorflow.keras.utils import to_categorical
 y = to_categorical(y)
 
+from sklearn.utils import shuffle
+xdata,y = shuffle(xdata,y)
 
 # Training an LSTM Layer for intent recognition
 from tensorflow.keras import models,layers
@@ -82,14 +83,31 @@ model.add(layers.Dense(40,activation='relu'))
 model.add(layers.Dense(20,activation='relu'))
 
 # add the output layer
-model.add(layers.Dense(len(intents),activation='sigmoid'))
+model.add(layers.Dense(len(intents),activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-model.fit(xdata,y,verbose=True,batch_size=100,epochs=10,shuffle=True)
+model.fit(xdata,y,verbose=True,batch_size=5,epochs=10,shuffle=True)
 
 print(model.evaluate(xdata,y))
+
 ############################################################################
 ### Save the trained model
 model.save("intent_recognizer.h5")
 
+#saving tokenizer
+import pickle
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tok, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+
+config = {}
+config['last_accuract'] = model.evaluate(xdata,y)[1]
+config['max_seq_len']=MAX_SEQUENCE_LENGTH
+config['embedding_dim']=EMBEDDING_DIM
+config['tokenizer_name']='tokenizer.pickle'
+config['model_name']="intent_recognizer.h5"
+
+
+file = open("chatbot_config.txt",'w')
+file.write(str(config))
+file.close()
